577d2bbdedc52d8fe46e51ca474d4e02141d9dcd
vinithvrangan <vinithrangan@gmail.com>
2026-02-11 11:11:39 +0530

    weatherapp created

---
+++ weatherapp/myapp/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+
+  const getWeather = () => {
+    if (!city) return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b386f19df433fa4ebc86a9dd4c3e44fa&units=metric`)
+      .then(res => res.json())
+      .then(data => setWeather(data));
+  };
+
+  return (
+    <div style={{ fontFamily: "Arial" }}>
+      <input 
+        value={city} 
+        onChange={(e) => setCity(e.target.value)} 
+        placeholder="Enter city"
+      />
+
+      <button onClick={getWeather}>Show Report</button>
+
+      {weather && weather.cod === "404" && (
+        <p style={{ color: "red" }}>City not found</p>
+      )}
+
+      {weather && weather.weather && (
+        <div style={{ marginTop: "10px" }}>
+          <h3>Weather Report for {weather.name}, {weather.sys.country}</h3>
+          <p><strong>Description:</strong> {weather.weather[0].description}</p>
+          <p><strong>Temperature:</strong> {weather.main.temp} °C</p>
+          <p><strong>Feels Like:</strong> {weather.main.feels_like} °C</p>
+          <p><strong>Humidity:</strong> {weather.main.humidity}%</p>
+          <p><strong>Pressure:</strong> {weather.main.pressure} hPa</p>
+          <p><strong>Wind Speed:</strong> {weather.wind.speed} m/s</p>
+          <p><strong>Latitude: </strong>{weather.coord.lat}°</p>
+          <p><strong>Longitude: </strong>{weather.coord.lon}°</p>
+          <p><strong>TimeZone: </strong>{weather.timezone}</p>
+        </div>
+      )}
+    </div>
+  );
+}
